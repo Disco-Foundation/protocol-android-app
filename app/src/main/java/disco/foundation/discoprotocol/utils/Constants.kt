b01a816df143539271747edf5e05e389abf67f97
dayana <dayana2301@gmail.com>
2022-06-19 16:03:34 +0100

    refactor: transaction request updated

--- app/src/main/java/disco/foundation/discoprotocol/utils/Constants.kt
+++ app/src/main/java/disco/foundation/discoprotocol/utils/Constants.kt
@@ -3,6 +3,6 @@ package disco.foundation.discoprotocol.utils
 const val ACCEPTED_MINT = "DISCO"
 val CHECK_IN_AMOUNT_TO_DEBIT = 0.000001.toBigDecimal()
 const val CAMERA_PERMISSION_CODE = 100
-const val BASE_ENDPOINT = "https://dafc-92-185-45-136.eu.ngrok.io/api/"//"https://api.disco.foundation/api/"
+const val BASE_ENDPOINT = "https://api.disco.foundation/api/"
 const val CHECK_IN = "check-in-new?"
 const val RECHARGE = "recharge?"
